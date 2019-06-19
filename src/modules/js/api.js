a1ed4ba20aeae1ec8465f47979a4da2b6255260c
luckzhu <luckzhu@1992@gmail.com>
2019-06-19 14:34:11 +0800

    首页热门商品

---
+++ src/modules/js/api.js
@@ -0,0 +1,17 @@
+let url = {
+    hotLists: 'index/hotLists'
+}
+
+//开发环境和真实环境的切换
+//let host = ''
+//现在用的是开发环境mock的接口数据
+let host = 'http://rap2api.taobao.org/app/mock/222071/'
+
+//把所有的url前面都加上host
+for (let key in url) {
+    if (url.hasOwnProperty(key)) {
+        url[key] = host + url[key];
+    }
+}
+
+export default url
