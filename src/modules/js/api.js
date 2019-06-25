de4f9f6adcd23833967cf4911eacf5bf3895e1f6
luckzhu <luckzhu@1992@gmail.com>
2019-06-25 18:04:26 +0800

    商品分类页

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,6 +1,9 @@
 let url = {
     hotLists: 'index/hotLists',
-    bannerLists: 'index/banner'
+    bannerLists: 'index/banner',
+    topList:'category/topList',
+    rank:'category/rank',
+    subList:'category/subList'
 }
 
 //开发环境和真实环境的切换
---
+++ src/pages/category/category.js
@@ -0,0 +1,57 @@
+import 'css/common.css'
+import './category.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+import Foot from 'components/Foot.vue'
+
+
+new Vue({
+    el: '#app',
+    components: {
+        Foot
+    },
+    data: {
+        topLists: null,
+        topIndex: 0,
+        rankData: null,
+        subData: null
+    },
+    created() {
+        this.getTopList()
+        this.getSublist(0,0)
+    },
+    methods: {
+        getTopList() {
+            axios.get(url.topList)
+                .then(res => {
+                    this.topLists = res.data.lists
+                }).catch(err => {
+                    console.log(err)
+                })
+        },
+        getSublist(id, index) { 
+            this.topIndex = index
+            if (index === 0) {
+                this.getRank()
+            } else {
+                axios.get(url.subList, { id }).then(res => {
+                    this.subData = res.data.data
+                })
+            }
+
+        },
+        getRank() {
+            axios.get(url.rank).then(res => {
+                this.rankData = res.data.data
+            })
+        }
+    },
+    filters:{
+        priceFormat(price){
+           return parseFloat(price).toFixed(2)
+        }
+    }
+
+})
