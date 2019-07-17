4b1a494dbf0522ee6d149e0ce10203ce753112d1
luckzhu <luckzhu1992@gmail.com>
2019-07-17 21:59:48 +0800

    将router放到单独的文件中

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,30 +1,5 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-Vue.use(Router)
-
-let routes = [{
-    path: '/',
-    component: require('./components/member.vue').default
-}, {
-    path: '/address',
-    component: require('./components/address.vue').default,
-    children: [{
-        path: '',
-        redirect: 'all'
-    }, {
-        path: 'all',
-        name: 'all',
-        component: require('./components/all.vue').default
-    }, {
-        path: 'form',
-        name: 'form',
-        component: require('./components/form.vue').default
-    }]
-}]
-
-let router = new Router({
-    routes
-})
+import router from './router'
 
 new Vue({
     el: '#app',
