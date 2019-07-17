9618480fee26de8286060febabd65ca55ed80183
luckzhu <luckzhu1992@gmail.com>
2019-07-17 22:16:29 +0800

    添加vuex

---
+++ src/pages/member/vuex/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+//使用vuex插件
+Vue.use(Vuex)
+
+import axios from 'axios'
+import url from 'js/api.js'
+import Address from 'js/addrService'
+
+//创建store实例
+const store = new Vuex.Store({
+    //类似于组件中data，但不能直接修改
+    state: {
+        lists: null
+    },
+    //修改state中的数据，同步的
+    mutations: {
+        init(state, lists) {
+            state.lists = lists
+        }
+    },
+    //当需要异步时，用来触发mutations
+    actions: {
+        getLists({ commit }) {
+            Address.lists().then(res => {
+                commit('init', res.data.lists)
+            })
+        }
+    })
+
+export default store
